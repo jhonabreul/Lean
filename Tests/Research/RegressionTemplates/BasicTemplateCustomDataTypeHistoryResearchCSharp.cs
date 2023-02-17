c95aad489accbd55702e8dc20cc47d3222b84e8b
Jhonathan Abreu <jdabreu25@gmail.com>
2023-02-17 17:37:13 -0400

    Minor changes

--- Tests/Research/RegressionTemplates/BasicTemplateCustomDataTypeHistoryResearchCSharp.cs
+++ Tests/Research/RegressionTemplates/BasicTemplateCustomDataTypeHistoryResearchCSharp.cs
@@ -28,19 +28,19 @@ namespace QuantConnect.Tests.Research.RegressionTemplates
         /// <remarks>Requires to be implemented last in the file <see cref="ResearchRegressionTests.UpdateResearchRegressionOutputInSourceFile"/>
         /// get should start from next line</remarks>
         public string ExpectedOutput =>
-            "{ \"cells\": [  {   \"cell_type\": \"markdown\",   \"id\": \"993eebad\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\"" +
+            "{ \"cells\": [  {   \"cell_type\": \"markdown\",   \"id\": \"e28a23ae\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\"" +
             ": null,     \"exception\": null,     \"start_time\": null,     \"status\": \"completed\"    },    \"tags\": []   },   \"source\": [    \"![QuantConnec" +
-            "t Logo](https://cdn.quantconnect.com/web/i/icon.png)\",    \"<hr>\"   ]  },  {   \"cell_type\": \"markdown\",   \"id\": \"5dcfcd64\",   \"metadata\": " +
+            "t Logo](https://cdn.quantconnect.com/web/i/icon.png)\",    \"<hr>\"   ]  },  {   \"cell_type\": \"markdown\",   \"id\": \"512dd871\",   \"metadata\": " +
             "{    \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\": null,     \"start_time\": null,     \"status\": \"completed\"" +
             "    },    \"tags\": []   },   \"source\": [    \"# Custom data history\"   ]  },  {   \"cell_type\": \"code\",   \"execution_count\": null,   \"id\": " +
-            "\"a3fb49a4\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\": null,     \"start_time\": null," +
+            "\"bb0c5d0a\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\": null,     \"start_time\": null," +
             "     \"status\": \"completed\"    },    \"tags\": [],    \"vscode\": {     \"languageId\": \"csharp\"    }   },   \"outputs\": [],   \"source\": [    " +
             "\"// We need to load assemblies at the start in their own cell\",    \"#load \\\"../Initialize.csx\\\"\"   ]  },  {   \"cell_type\": \"code\",   \"exe" +
-            "cution_count\": null,   \"id\": \"65c15cd5\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\":" +
+            "cution_count\": null,   \"id\": \"f972f2ce\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\":" +
             " null,     \"start_time\": null,     \"status\": \"completed\"    },    \"tags\": [],    \"vscode\": {     \"languageId\": \"csharp\"    }   },   \"ou" +
             "tputs\": [],   \"source\": [    \"// Initialize Lean Engine.\",    \"#load \\\"../QuantConnect.csx\\\"\",    \"\",    \"using System.Globalization;\"," +
             "    \"using QuantConnect;\",    \"using QuantConnect.Data;\",    \"using QuantConnect.Algorithm;\",    \"using QuantConnect.Research;\"   ]  },  {   \"" +
-            "cell_type\": \"code\",   \"execution_count\": null,   \"id\": \"b050a297\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_ti" +
+            "cell_type\": \"code\",   \"execution_count\": null,   \"id\": \"5b180ed7\",   \"metadata\": {    \"papermill\": {     \"duration\": null,     \"end_ti" +
             "me\": null,     \"exception\": null,     \"start_time\": null,     \"status\": \"completed\"    },    \"tags\": [],    \"vscode\": {     \"languageId\"" +
             ": \"csharp\"    }   },   \"outputs\": [],   \"source\": [    \"class CustomDataType : DynamicData\",    \"{\",    \"    public decimal Open;\",    \" " +
             "   public decimal High;\",    \"    public decimal Low;\",    \"    public decimal Close;\",    \"\",    \"    public override SubscriptionDataSource " +
@@ -53,18 +53,18 @@ namespace QuantConnect.Tests.Research.RegressionTemplates
             "fo.InvariantCulture).AddHours(20),\",    \"                Value = csv[4].ToDecimal(),\",    \"                Open = csv[1].ToDecimal(),\",    \"    " +
             "            High = csv[2].ToDecimal(),\",    \"                Low = csv[3].ToDecimal(),\",    \"                Close = csv[4].ToDecimal()\",    \"  " +
             "          };\",    \"\",    \"            return data;\",    \"        }\",    \"        catch\",    \"        {\",    \"            return null;\",  " +
-            "  \"        }\",    \"    }\",    \"}\"   ]  },  {   \"cell_type\": \"code\",   \"execution_count\": null,   \"id\": \"28360142\",   \"metadata\": {  " +
+            "  \"        }\",    \"    }\",    \"}\"   ]  },  {   \"cell_type\": \"code\",   \"execution_count\": null,   \"id\": \"e7608067\",   \"metadata\": {  " +
             "  \"papermill\": {     \"duration\": null,     \"end_time\": null,     \"exception\": null,     \"start_time\": null,     \"status\": \"completed\"   " +
             " },    \"tags\": [],    \"vscode\": {     \"languageId\": \"csharp\"    }   },   \"outputs\": [],   \"source\": [    \"var qb = new QuantBook();\",   " +
             " \"var symbol = qb.AddData<CustomDataType>(\\\"CustomDataType\\\", Resolution.Hour).Symbol;\",    \"\",    \"var start = new DateTime(2017, 8, 20);\"," +
-            "    \"var end = start.AddHours(48);\",    \"var history = qb.History<CustomDataType>(symbol, start, end, Resolution.Hour);\",    \"\",    \"foreach (v" +
-            "ar data in history.Take(5))\",    \"{\",    \"    Console.WriteLine(data.ToString());\",    \"}\"   ]  } ], \"metadata\": {  \"kernelspec\": {   \"dis" +
-            "play_name\": \"Foundation-C#-Default\",   \"language\": \"C#\",   \"name\": \"csharp\"  },  \"language_info\": {   \"file_extension\": \".cs\",   \"mi" +
-            "metype\": \"text/x-csharp\",   \"name\": \"C#\",   \"pygments_lexer\": \"csharp\",   \"version\": \"10.0\"  },  \"papermill\": {   \"default_parameter" +
-            "s\": {},   \"duration\": 0.806508,   \"end_time\": \"2023-02-17T19:22:14.110548\",   \"environment_variables\": {},   \"exception\": null,   \"input_p" +
-            "ath\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\RegressionTemplates\\\\BasicTemplateCustomDataTypeHistor" +
-            "yResearchCSharp.ipynb\",   \"output_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\RegressionTemplates" +
-            "\\\\BasicTemplateCustomDataTypeHistoryResearchCSharp-output.ipynb\",   \"parameters\": {},   \"start_time\": \"2023-02-17T19:22:13.304040\",   \"versi" +
-            "on\": \"2.4.0\"  } }, \"nbformat\": 4, \"nbformat_minor\": 5}";
+            "    \"var end = start.AddHours(48);\",    \"var history = qb.History<CustomDataType>(symbol, start, end, Resolution.Hour).ToList();\",    \"\",    \"i" +
+            "f (history.Count == 0)\",    \"{\",    \"    throw new Exception(\\\"No history data returned\\\");\",    \"}\"   ]  } ], \"metadata\": {  \"kernelspe" +
+            "c\": {   \"display_name\": \"Foundation-C#-Default\",   \"language\": \"C#\",   \"name\": \"csharp\"  },  \"language_info\": {   \"file_extension\": \"" +
+            ".cs\",   \"mimetype\": \"text/x-csharp\",   \"name\": \"C#\",   \"pygments_lexer\": \"csharp\",   \"version\": \"10.0\"  },  \"papermill\": {   \"defa" +
+            "ult_parameters\": {},   \"duration\": 1.654161,   \"end_time\": \"2023-02-17T21:33:05.325357\",   \"environment_variables\": {},   \"exception\": null" +
+            ",   \"input_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\RegressionTemplates\\\\BasicTemplateCustomD" +
+            "ataTypeHistoryResearchCSharp.ipynb\",   \"output_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\Regres" +
+            "sionTemplates\\\\BasicTemplateCustomDataTypeHistoryResearchCSharp-output.ipynb\",   \"parameters\": {},   \"start_time\": \"2023-02-17T21:33:03.671196" +
+            "\",   \"version\": \"2.4.0\"  } }, \"nbformat\": 4, \"nbformat_minor\": 5}";
     }
 }
