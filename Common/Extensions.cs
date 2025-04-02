d4c8580dfffb53bbdbcdc5e6093a40c455f347b7
Jhonathan Abreu <jdabreu25@gmail.com>
2025-04-02 17:51:08 -0400

    Reset Index.ManualSetIsTradable when security is removed

--- Common/Extensions.cs
+++ Common/Extensions.cs
@@ -3914,10 +3914,25 @@ namespace QuantConnect
         /// </summary>
         public static void MakeTradable(this Security security, bool forceInternal = false)
         {
-            if (security.Type != SecurityType.Index || (security as Securities.Index.Index).ManualSetIsTradable)
+            if ((security.Type != SecurityType.Index || (security as Securities.Index.Index).ManualSetIsTradable) &&
+                (forceInternal || security.Subscriptions.Any(config => !config.IsInternalFeed)))
             {
-                security.IsTradable = forceInternal || security.Subscriptions.Any(config => !config.IsInternalFeed);
+                security.IsTradable = true;
             }
+        }
+
+        /// <summary>
+        /// Helper method to set the <see cref="Security.IsTradable"/> property to <code>false</code>
+        /// when a security is being removed from the algorithm or delisted
+        /// </summary>
+        /// <param name="security"></param>
+        public static void MakeNonTradable(this Security security)
+        {
+            if (security is Securities.Index.Index index)
+            {
+                index.ManualSetIsTradable = false;
+            }
+            security.IsTradable = false;
         }
 
         /// <summary>
